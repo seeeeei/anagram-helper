59fe5de806e2974ef183b725287cc8371ff68e2d
朱婧敏 <454254706@163.com>
2017-04-01 00:53:31 -0700

    finished

--- generate.py
+++ generate.py
@@ -1,8 +1,23 @@
-import dict.dict
+from dict import dict
+import sys
   
 letters = sys.argv[ 1: ]
 used = {}
 for i in range( 0, len( letters ) ):
 	used[ i ] = False
-word = ""
 
+def rec( depth, word ):
+	if depth:
+		for i in range( 0, len( letters ) ):
+			if not used[ i ]:
+				used[ i ] = True
+				rec( depth - 1, word + letters[ i ] )
+				used[ i ] = False
+	else:
+		if word in dict:
+			print word
+
+n = len( letters )
+while n>=3:
+	rec( n, "" )
+	n -= 1
